60f51ec0a2f8493d6397913111fa92d4b702e0d2
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-04-06 15:09:40 +0300

    finsh

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const server = express();
+const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
 
 server.use(cors());
@@ -15,27 +16,66 @@ server.get('/', (req, res) =>
 );
 
 server.get('/location', (req, res) => {
-  let serch = req.query.city;
-  let data = require('./data/geo.json');
-  let resulte = new Location(serch, data);
-  res.status(200).send(resulte);
+  let search = req.query.city;
+  const key = process.env.GEOCODE_API_KEY;
+  superagent
+    .get(
+      `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${search}&format=json`
+    )
+    .then((data) => res.status(200).send(new Location(search, data.body)));
 });
 
 server.get('/weather', (req, res) => {
-  // let search = req.query.city;
-  let data = require('./data/weather.json').data;
-  let resulte = [];
-  data.forEach((obj) => {
-    resulte.push(new Weather(obj));
-  });
-  res.status(200).send(resulte);
+  let search = req.query.search_query;
+  let key = process.env.WEATHER_API_KEY;
+  superagent
+    .get(
+      `https://api.weatherbit.io/v2.0/forecast/daily?city=${search}&key=${key}`
+    )
+    .then((data) =>
+      res.status(200).send(data.body.data.map((obj) => new Weather(obj)))
+    );
 });
+
+server.get('/trails', (req, res) => {
+  let search = req.query.search_query;
+  const keyLoction = process.env.GEOCODE_API_KEY;
+  const key = process.env.TRAIL_API_KEY;
+  //// we will take the lat and lon from the location in locationIQ api the pass it to the Hike api
+  superagent
+    .get(
+      `https://eu1.locationiq.com/v1/search.php?key=${keyLoction}&q=${search}&format=json`
+    )
+    .then((data) => {
+      let location = data.body[0];
+      superagent
+        .get(
+          `https://www.hikingproject.com/data/get-trails?lat=${location.lat}&lon=${location.lon}&maxDistance=10&key=${key}`
+        )
+        .then((data) =>
+          res.status(200).send(data.body.trails.map((obj) => new Hike(obj)))
+        );
+    });
+});
+
+function Hike(object) {
+  this.name = object.name;
+  this.location = object.location;
+  this.length = object.length;
+  this.stars = object.stars;
+  this.star_votes = object.starVotes;
+  this.summary = object.summary;
+  this.trial_url = object.url;
+  this.conditions = object.conditionDetails;
+  this.condition_date = object.conditionDate.split(' ')[0];
+  this.condition_time = object.conditionDate.split(' ')[1];
+}
 
 function Location(query, data) {
   (this.search_query = query),
-  (this.formateed_query = data[0].display_name),
-  (this.latitude = data[0].lat),
-  (this.longitude = data[0].lon);
+    (this.formateed_query = data[0].display_name),
+    (this.latitude = data[0].lat),
+    (this.longitude = data[0].lon);
 }
 
 const mounths = {
@@ -57,8 +97,7 @@ function Weather(data) {
   /// change the mounth from noumber to the name of it
   let newDate = data.datetime.split('-');
   newDate[1] = mounths[newDate[1]];
-  (this.description = data.weather.description),
-  (this.time = newDate.join('-'));
+  (this.forecast = data.weather.description), (this.time = newDate.join('-'));
 }
 
 server.get('*', (req, res) => res.status(404).send('NOT FOUND'));
